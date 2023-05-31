5bb0ea705d9c63b1d3c06c362fff6022a5eeed78
JarbasAi <jarbasai@mailfence.com>
2023-05-31 15:19:31 +0100

    update public servers

--- ovos_stt_plugin_server/__init__.py
+++ ovos_stt_plugin_server/__init__.py
@@ -2,22 +2,33 @@ from queue import Queue
 from uuid import uuid4
 
 import requests
-from ovos_plugin_manager.stt import STT
-from ovos_plugin_manager.stt import StreamingSTT, StreamThread
+from ovos_utils.log import LOG
+from ovos_plugin_manager.stt import STT, StreamingSTT, StreamThread
 
 
 class OVOSHTTPServerSTT(STT):
     """STT interface for the OVOS-HTTP-STT-Server"""
+    public_servers = [
+        "https://stt.openvoiceos.org/stt"
+    ]
 
     def __init__(self, config=None):
         super().__init__(config)
-        self.url = self.config.get("url") or "https://stt.openvoiceos.org/stt"
+        self.urls = self.config.get("url") or self.config.get("urls") or self.public_servers
+        if not isinstance(self.urls, list):
+            self.urls = [self.urls]
 
     def execute(self, audio, language=None):
-        self.response = requests.post(self.url, data=audio.get_wav_data(),
-                                      headers={"Content-Type": "audio/wav"},
-                                      params={"lang": language or self.lang})
-        return self.response.text if self.response else None
+        for url in self.urls:
+            try:
+                self.response = requests.post(url, data=audio.get_wav_data(),
+                                              headers={"Content-Type": "audio/wav"},
+                                              params={"lang": language or self.lang})
+                if self.response:
+                    return self.response.text
+            except:
+                pass
+            LOG.error(f"STT request to {url} failed")
 
 
 class OVOSHTTPStreamServerStreamThread(StreamThread):
