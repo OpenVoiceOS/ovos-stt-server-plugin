2aabb27fe74dd750c377384f9ea376dae17ec50d
Daniel McKnight <daniel@neon.ai>
2024-06-03 15:21:32 -0700

    Troubleshooting error handling

--- ovos_stt_plugin_server/__init__.py
+++ ovos_stt_plugin_server/__init__.py
@@ -48,8 +48,9 @@ class OVOSHTTPServerSTT(STT):
                                          headers={"Content-Type": "audio/wav"},
                                          params={"lang": language or self.lang},
                                          verify=self.verify_ssl)
-                if not response:
-                    LOG.error(f"No response from {url}")
+                LOG.debug(f"response={response}")
+                if response is None:
+                    LOG.error(f"None response from {url}")
                 elif not response.ok:
                     LOG.error(f"{response.status_code} response from {url}")
                 else:
