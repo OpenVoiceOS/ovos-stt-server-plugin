52638a9e624b0d82cbaeae866a1a72f1d07de9f5
Daniel McKnight <daniel@neon.ai>
2024-06-03 15:03:05 -0700

    Update response handling for better logging

--- ovos_stt_plugin_server/__init__.py
+++ ovos_stt_plugin_server/__init__.py
@@ -48,8 +48,11 @@ class OVOSHTTPServerSTT(STT):
                                          headers={"Content-Type": "audio/wav"},
                                          params={"lang": language or self.lang},
                                          verify=self.verify_ssl)
-                LOG.debug(f"Response={response.status_code}")
-                if response:
+                if not response:
+                    LOG.error(f"No response from {url}")
+                elif not response.ok:
+                    LOG.error(f"{response.status_code} response from {url}")
+                else:
                     return response.text
             except Exception as e:
                 LOG.exception(e)
